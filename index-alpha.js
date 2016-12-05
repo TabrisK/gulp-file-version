26eb8aea35a5ad414ffae30546eef2c958a31311
TabrisK <Administrator@CLW004.deren.com.cn>
2016-12-05 10:09:17 +0800

    remove console.log('balabala')

--- index-alpha.js
+++ index-alpha.js
@@ -29,7 +29,7 @@ function fv(regexp, opts) {
         var parsedPath = parsePath(file.relative);
         var content = file.contents.toString("utf8");
         var srcCollection = [];
-        var _regexp = regexp?new RegExp(regexp):new RegExp(/ src=['"]{1}([\/\w\.]*.js)['"]{1}| href=['"]{1}([\/\w\.]*.css)['"]{1}/g);
+        var _regexp = regexp?new RegExp(regexp):new RegExp(/ src=['"]{1}([\/\w\.]*.js[?v=\w]*)['"]{1}| href=['"]{1}([\/\w\.]*.css[?v=\w]*)['"]{1}/g);
         var r = _regexp;
         var temp = r.exec(content);
         while(temp){
@@ -37,7 +37,6 @@ function fv(regexp, opts) {
             srcCollection.push(temp);
             temp = r.exec(content);
         }
-        console.log(process.cwd());
         srcCollection.map(function(v){
             var basePath = _opts.base?Path.join(process.cwd(), _opts.base):file.dirname;
             var srcPath = Path.join(basePath, v);
