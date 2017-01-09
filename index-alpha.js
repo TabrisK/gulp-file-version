5256833918b812bcf09743e4f35b31bc4ad560ef
TabrisK <Administrator@CLW004.deren.com.cn>
2017-01-09 11:25:41 +0800

    change the regexp who match the src and href files.change the replace regexp.

--- index-alpha.js
+++ index-alpha.js
@@ -37,7 +37,7 @@ function fv(arg1, arg2) {
         var parsedPath = parsePath(file.relative);
         var content = file.contents.toString("utf8");
         var srcCollection = [];
-        var _regexp = regexp ? new RegExp(regexp) : new RegExp(/ src=['"]{1}([\/\w\.-]*.js\?v=\w*|[\/\w\.-]*.js)['"]{1}| href=['"]{1}([\/\w\.-]*.css\?v=\w*|[\/\w\.-]*.css)['"]{1}/g);
+        var _regexp = regexp ? new RegExp(regexp) : new RegExp(/ src=['"]{1}([\/\w\.-]*.js)(\?v=\w*)?['"]{1}| href=['"]{1}([\/\w\.-]*.css)(\?v=\w*)?['"]{1}/g);
         var r = _regexp;
         var temp = r.exec(content);
         while (temp) {
@@ -50,14 +50,14 @@ function fv(arg1, arg2) {
             srcCollection.push(temp);
             temp = r.exec(content);
         }
-        srcCollection.map(function (v) {
+        srcCollection.map(function (v) {//v is a string, which is the file relative path
             var basePath = _opts.base ? Path.join(process.cwd(), _opts.base) : file.dirname;
             var srcPath = Path.join(basePath, v);
             var suffix;
             var srcContent = fs.readFileSync(srcPath);
             if (_opts.Hash) {
-                suffix = crypto.createHash(_opts.Hash).update(srcContent).digest("hex").substring(0,10);
-            }else {
+                suffix = crypto.createHash(_opts.Hash).update(srcContent).digest("hex").substring(0, 10);
+            } else {
                 var count = 0;
                 for (var index in srcContent) {
                     if (typeof(srcContent[index]) == "number")
@@ -65,7 +65,11 @@ function fv(arg1, arg2) {
                 }
                 suffix = count.toString(16);
             }
-            content = content.replace(new RegExp(v, "g"), v + vInfo + suffix);
+            console.log(v + "\\?v=\\w*" + "|" + v);
+            console.log(v + vInfo + suffix);
+            content = content.replace(
+                new RegExp(v + "\\?v=\\w*" + "|" + v, "g"),
+                v + vInfo + suffix);
         });
         file.contents = Buffer.from(content);
         callback(null, file);
